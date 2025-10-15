fe77ce98804524bd6af27e44cdf690bc1765ce45
sheshkaran <sksolanki1366@gmail.com>
2025-10-15 22:00:34 +0530

    Initial commit - Radha Names App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Name from "./name";
+
+function App() {
+  
+  return (
+    <>
+      <Name/>
+    </>
+  );
+}
+
+export default App;
