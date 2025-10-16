2c3d45d20bb9584420aa5ebe32be9dc5388cd27b
sheshkaran <sksolanki1366@gmail.com>
2025-10-16 15:04:22 +0530

    some change in  App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "@mui/material/Button";
-// import Name from "./name";
+import Name from "./name";
 import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
 import WeatherApp from "./WeatherApp";
